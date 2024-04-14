2ba5635df35a6b55a3b680e2c9a061c9ff04ac8b
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-04-14 16:11:05 +0500

    132
    132

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import s from "../styles/Navbar.module.css"
 import Image from 'next/image'
 import { IoCartOutline } from "react-icons/io5";
@@ -11,6 +11,24 @@ import img1 from "../images/logo1.png"
 
 import { IoLocationSharp } from "react-icons/io5";
 export default function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset >= 100) {
+        console.log(window.pageYOffset);
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <>
 <div className={s.navbar}>
@@ -20,13 +38,23 @@ export default function Navbar() {
     </div>
 </div>
 <div className={s.status}>
-<div className={s.logo}>
+  {!showNavbar?(<><div className={s.search}>
+<div className={s.red}>
+<CgMenuGridR style={{fontSize:'30px'}} />
+<span>Каталог товаров</span>
+</div>
+<div className={s.search_inp}>
+<LiaSearchSolid style={{marginLeft:'20px',color:'red',fontSize:'20px'}} />
+    <input type="text" placeholder='Поиск среди 8 637 350 компонентов, электроники, приборов, электрики, автозапчастей, инструментов, сантехники…' />
+</div>
+</div></>):(<div className={s.logo}>
 <Image src={img1} alt="" />
 <div className={s.loc}>
 <IoLocationSharp style={{marginTop:'5px'}} />
         <p><strong>Россия</strong> <br /> Магазины и оптовые отделы</p>
 </div>
-</div>
+</div>)}
+
 <div className={s.kirish}>
 <div className={s.order}>
 <FaRegUserCircle style={{fontSize:'30px'}}  /><br />
