c0b1638e7c7e55d667ae05d18826ea5789bb2724
Fotimafgyt <159242143+Fotimafgyt@users.noreply.github.com>
2024-04-24 17:12:20 +0500

    1212
    1212

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -5,6 +5,7 @@ import { IoCartOutline } from "react-icons/io5";
 import { CgMenuGridR } from "react-icons/cg";
 import { FaRegUserCircle } from "react-icons/fa";
 import { LiaSearchSolid } from "react-icons/lia";
+import { MdArrowForwardIos } from "react-icons/md";
 import { BsFillBoxSeamFill } from "react-icons/bs";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
 import img1 from "../images/logo1.png"
@@ -40,18 +41,77 @@ export default function Navbar() {
 </div>
 <div className={s.status}>
   {!showNavbar?(<><div id={s.search1} className={s.search}>
-<div className={s.red}>
+<div onMouseLeave={()=>{
+  document.querySelector("#menu").style="display:none"
+
+}} onClick={()=>{
+  document.querySelector("#menu").style="display:block"
+}} className={s.red}>
 <CgMenuGridR style={{fontSize:'30px'}} />
 <span>Каталог товаров</span>
 
-<div className={s.catalog_menu}>
+<div id='menu' className={s.catalog_menu}>
   <div className={s.catalog_toolbar}>
-    <div className={s.accor}></div>
-    <div className={s.sub_category}>
-      
+   <div className={s.bigac}>
+   <div className={s.accor}>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom").style='display:grid'
+      }} className={s.accordf}>
+        <h3 >Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom").style='opacity:1;'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
     </div>
+   </div>
+   <div className={s.yoq}>
+   <div id='nom' className={s.sub_category }>
+      <div  onMouseLeave={()=>{
+        document.querySelector("#nom").style='opacity:0;'
+
+}} className={s.modul}>
+        <ul>
+          <li >Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+        
+        </ul>
+      </div>
+    </div>
+   </div>
   </div>
 </div>
+
+
 </div>
 <div className={s.search_inp}>
 <LiaSearchSolid style={{marginLeft:'20px',color:'red',fontSize:'20px'}} />
@@ -86,8 +146,11 @@ export default function Navbar() {
 </div>
 </div>
 </div>
-<div className={s.search}>
-<div className={s.red}>
+<div   className={s.search}>
+<div  onClick={()=>{
+  document.querySelector("#nom1").style="opacity:0"
+  document.querySelector("#menu1").style="display:block"
+}} className={s.red}>
 <CgMenuGridR style={{fontSize:'30px'}} />
 <span>Каталог товаров</span>
 </div>
@@ -95,7 +158,68 @@ export default function Navbar() {
 <LiaSearchSolid style={{marginLeft:'20px',color:'red',fontSize:'20px'}} />
     <input type="text" placeholder='Поиск среди 8 637 350 компонентов, электроники, приборов, электрики, автозапчастей, инструментов, сантехники…' />
 </div>
+<div id='menu1'  onMouseLeave={()=>{
+  document.querySelector("#menu1").style="display:none"
+}} className={s.catalog_menu}>
+  <div className={s.catalog_toolbar}>
+   <div className={s.bigac}>
+   <div className={s.accor}>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom1").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
 
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom1").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom1").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom1").style='display:grid'
+      }} className={s.accordf}>
+        <h3 >Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+      <div  onMouseEnter={()=>{
+        document.querySelector("#nom1").style='display:grid'
+      }} className={s.accordf}>
+        <h3>Электронные компоненты</h3>
+        <MdArrowForwardIos className={s.forward} />
+      </div>
+    </div>
+   </div>
+   <div className={s.yoq}>
+   <div id='nom1' className={s.sub_category }>
+      <div  onMouseLeave={()=>{
+        document.querySelector("#nom1").style='opacity:0;'
+
+}} className={s.modul}>
+        <ul>
+          <li >Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+          <li>Промышленные разъёмы круглые <sub>91302</sub></li>
+        
+        </ul>
+      </div>
+    </div>
+   </div>
+  </div>
+</div>
 </div>
 
     </>
