1537e6a0a7d43b35e3c3f9579034f409e0e2db78
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-06-14 15:35:31 +0500

    qwe
    ewq

--- src/pages/host.js
+++ src/pages/host.js
@@ -1,5 +1,5 @@
-var url="https://test.codeup.uz"
-// var url ='https://mysklad-back-1.onrender.com/'
+// var url="https://test.codeup.uz"
+var url ='https://mysklad-back.onrender.com'
 export default function host() {
   return url 
 }
